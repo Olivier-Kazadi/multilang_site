59deaa3916070aa50f714f6a090831dd4eea23d3
Olivier-Kazadi <63251013+Olivier-Kazadi@users.noreply.github.com>
2024-06-19 16:09:22 +0200

    Update settings.py

--- multilang_site/multilang_site/settings.py
+++ multilang_site/multilang_site/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from django.utils.translation import gettext_lazy as _
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-^rx2&da-**_3)m2l33*hf005efc#2&(=b1211n#pg!dm)(1vcf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'multilang-site-bk9i.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
